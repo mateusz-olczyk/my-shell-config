e5b0cd971f82c142d41178bfc723027aceea7b67
Mateusz Olczyk <mateusz-olczyk@outlook.com>
2024-12-05 19:49:53 +0100

    Add OMZ and P10K to the installation

--- install.sh
+++ install.sh
@@ -2,24 +2,26 @@
 
 set -o errexit -o pipefail -o nounset
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OMZ_CUSTOM_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+P10K_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ZSHRC_PATH=$HOME/.zshrc
 
-
-
-### APT PACKAGES ###
-
-if [[ $(whoami) == "root" ]]; then
+if [ $(whoami) = "root" ]; then
     sudo() { "$@"; }
 fi
 
 sudo apt update
 
-PACKAGES=(
+APT_PACKAGES=(
     bat:batcat
+    git
+    sed
     tmux
+    wget
     zsh
 )
 
-for PACKAGE_DEF in ${PACKAGES[@]}; do
+for PACKAGE_DEF in ${APT_PACKAGES[@]}; do
     PACKAGE_NAME=${PACKAGE_DEF/:*/}
     APP_NAME=${PACKAGE_DEF/*:/}
 
@@ -27,3 +29,13 @@ for PACKAGE_DEF in ${PACKAGES[@]}; do
         apt install -y $PACKAGE_NAME
     fi
 done
+
+if ! [ -d $OMZ_CUSTOM_DIR ]; then
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+fi
+
+if ! [ -d $P10K_DIR ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $P10K_DIR
+fi
+
+sed -i 's|^ZSH_THEME=".*"$|ZSH_THEME="powerlevel10k/powerlevel10k"|' $ZSHRC_PATH
